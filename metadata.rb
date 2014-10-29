0f0a15e2a9daaa0d5141dd908edbbcc8eb3c86a2
wangqi <to.wangqi@gmail.com>
2014-10-29 18:44:16 +0800

    Add init version

---
+++ metadata.rb
@@ -0,0 +1,29 @@
+name             "elasticsearch"
+
+maintainer       "karmi"
+maintainer_email "karmi@karmi.cz"
+license          "Apache"
+description      "Installs and configures elasticsearch"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
+version          "0.3.11"
+
+depends 'apt'
+depends 'vim'
+depends 'ark', '>= 0.2.4'
+depends 'java', '>= 1.7.0'
+depends 'monit'
+
+recommends 'build-essential'
+recommends 'xml'
+recommends 'java'
+recommends 'monit'
+
+provides 'elasticsearch'
+provides 'elasticsearch::data'
+provides 'elasticsearch::ebs'
+provides 'elasticsearch::aws'
+provides 'elasticsearch::nginx'
+provides 'elasticsearch::proxy'
+provides 'elasticsearch::plugins'
+provides 'elasticsearch::monit'
+provides 'elasticsearch::search_discovery'
